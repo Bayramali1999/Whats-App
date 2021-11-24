db3e90a913534c9b9cb08211faeaed7234cab879
Bayramali1999 <babdiyev361@gmail.com>
2021-11-25 00:37:53 +0500

    added contact success

---
+++ app/src/main/java/uz/soft/whatsapp/ui/RequestsFragment.java
@@ -0,0 +1,22 @@
+package uz.soft.whatsapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import uz.soft.whatsapp.R;
+
+
+public class RequestsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_requests, container, false);
+    }
+}
